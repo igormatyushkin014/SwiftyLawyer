8be3538b3abc9433f4d0e741f6f8e6757fbf5439
Igor Matyushkin <igormatyushkin014@gmail.com>
2018-06-24 01:28:52 +0300

    Added demo project

---
+++ SwiftyLawyer/SwiftyLawyer/SwiftyLawyer/SwiftyLawyer.h
@@ -0,0 +1,19 @@
+//
+//  SwiftyLawyer.h
+//  SwiftyLawyer
+//
+//  Created by Igor Matyushkin on 24.06.2018.
+//  Copyright © 2018 Igor Matyushkin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for SwiftyLawyer.
+FOUNDATION_EXPORT double SwiftyLawyerVersionNumber;
+
+//! Project version string for SwiftyLawyer.
+FOUNDATION_EXPORT const unsigned char SwiftyLawyerVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SwiftyLawyer/PublicHeader.h>
+
+
---
+++ SwiftyLawyer/SwiftyLawyerDemo/SwiftyLawyerDemo/ViewControllers/Main/MainViewController.swift
@@ -0,0 +1,47 @@
+//
+//  MainViewController.swift
+//  SwiftyLawyerDemo
+//
+//  Created by Igor Matyushkin on 24.06.2018.
+//  Copyright © 2018 Igor Matyushkin. All rights reserved.
+//
+
+import UIKit
+
+class MainViewController: UIViewController {
+    
+    // MARK: Class variables & properties
+    
+    // MARK: Public class methods
+    
+    // MARK: Private class methods
+    
+    // MARK: Initializers
+    
+    // MARK: Deinitializer
+    
+    deinit {
+    }
+    
+    // MARK: Outlets
+    
+    // MARK: Object variables & properties
+    
+    // MARK: Public object methods
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    // MARK: Private object methods
+    
+    // MARK: Actions
+    
+    // MARK: Protocol implementation
+    
+}
